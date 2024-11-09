a69f352c30bcaec08c07c2e2130b2c36f7d42393
kakitab <karolinakita.70@gmail.com>
2024-11-09 11:22:13 +0100

    nowe laby

--- pp7/lab_7.3.py
+++ pp7/lab_7.3.py
@@ -1,27 +1,58 @@
+# import random
+#
+# number=random.randint(1,10)
+# guess = int(input("Guess a number (1-10): "))
+#
+# if guess == number:
+#     print("Congrats! You guessed the number!")
+# elif guess > number:
+#     print("Niestety nie,ale masz jeszcze dwie próby! Liczba jest mniejsza..")
+# elif guess < number:
+#     print("Niestety nie, ale masz jeszcze dwie próby! Liczba jest większa..")
+#
+# guess = int(input("Guess a number (1-10): "))
+# if guess == number:
+#     print("Congrats! You guessed the number!")
+# elif guess > number:
+#     print("Niestety nie,ale masz jeszcze jedną próbę! Liczba jest mniejsza..")
+# elif guess < number:
+#     print("Niestety nie, ale masz jeszcze jedną próbę! Liczba jest większa..")
+#
+# guess = int(input("Guess a number (1-10): "))
+# if guess == number:
+#     print("Congrats! You guessed the number!")
+# elif guess > number:
+#     print("Niestety nie, liczba o której myślałem to", number)
+# elif guess < number:
+#     print("Niestety nie, liczba o której myślałem to", number)
+#
+
 import random
 
-number=random.randint(1,10)
-guess = int(input("Guess a number (1-10): "))
+number1=random.randint(1,10)
+msg="Guess a number (1-10): "
+guess1=int(input(msg))
 
-if guess == number:
-    print("Congrats! You guessed the number!")
-elif guess > number:
-    print("Niestety nie,ale masz jeszcze dwie próby! Liczba jest mniejsza..")
-elif guess < number:
-    print("Niestety nie, ale masz jeszcze dwie próby! Liczba jest większa..")
-
-guess = int(input("Guess a number (1-10): "))
-if guess == number:
-    print("Congrats! You guessed the number!")
-elif guess > number:
-    print("Niestety nie,ale masz jeszcze jedną próbę! Liczba jest mniejsza..")
-elif guess < number:
-    print("Niestety nie, ale masz jeszcze jedną próbę! Liczba jest większa..")
-
-guess = int(input("Guess a number (1-10): "))
-if guess == number:
-    print("Congrats! You guessed the number!")
-elif guess > number:
-    print("Niestety nie, liczba o której myślałem to", number)
-elif guess < number:
-    print("Niestety nie, liczba o której myślałem to", number)
+if guess1==number1:
+    print("Brawo! Zgadłeś!")
+else:
+    msg = "Masz kolejną szansę, moja liczba jest "
+    if number1 % 2 == 0:
+        msg += "parzysta "
+    else:
+        msg+= "nieparzysta "
+    guess1=int(input(msg))
+    if guess1==number1:
+        print("Brawo udało się zgadnąć za drugim razem!")
+    else:
+        msg="Masz ostatnią szansę. Moja liczba jest "
+        if number1 > 5:
+            msg += "większa niż"
+        else:
+            msg+= "mniejsza lub równa "
+        msg+="pięć "
+        guess1 = int(input(msg))
+        if guess1 == number1:
+            print("Brawo udało się zgadnąć za trzecim razem!")
+        else:
+            print("Niestety moja liczba to " + str(number1))
